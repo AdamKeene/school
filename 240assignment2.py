5450373f6396a005e39cbc1a33c3fb8c808e30b8
Adam :) <51837429+AdamKeene@users.noreply.github.com>
2024-10-06 13:08:09 -0700

    more 2

--- 240assignment2.py
+++ 240assignment2.py
@@ -29,6 +29,8 @@ class Stack:
                 while (not self.is_empty() and self.peek() in precedence and precedence[self.peek()] >= precedence[token]):
                     output.append(self.pop())
                 self.push(token)
+            else:
+                raise Exception('NaN')
         
         while not self.is_empty():
             output.append(self.pop())
@@ -59,3 +61,47 @@ stack = Stack()
 postfix_exp = stack.postfix(tokens)
 result = stack.evaluate_postfix(postfix_exp)
 print(result)
+
+class Queue:
+    default_capacity = 10
+    def __init__(self):
+        self._data = [None] * Queue.default_capacity
+        self._size = 0
+        self._front = 0
+    
+    def enqueue(self, e):
+        if self._size == len(self._data):
+            self._resize(2 * len(self._data)) # double the array size
+        avail = (self. front + self. size) % len(self._data)
+        self.data[avail] = e
+        self.size += 1
+    
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception('Queue is empty')
+        answer = self._data[self._front]
+        self._data[self._front] = None # help garbage collection
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+    
+    def poll(self):
+        if self.is_empty():
+            raise Exception('Queue is empty')
+        return self._data[self._front]
+    
+    def size(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+    
+    def resize(self, cap): # we assume cap >= len(self)
+    #Resize to a new list of capacity >= len(self).
+        old = self._data # keep track of existing list 
+        self._data = [None] * cap # allocate list with new capacity
+        walk = self._front
+        for k in range(self._size): # only consider existing elements
+            self._data[k] = old[walk] # intentionally shift indices
+            walk = (1 + walk) % len(old) # use old size as modulus
+        self._front = 0
