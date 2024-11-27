ee606e68ca99645e5715b5311825f61388bf3bed
Adam :) <51837429+AdamKeene@users.noreply.github.com>
2024-11-27 14:03:45 -0800

    working??

--- applied_info_retrieval/SearchingScoring.py
+++ applied_info_retrieval/SearchingScoring.py
@@ -11,13 +11,14 @@ def cosine(d, q):
     for i in range(length):
         if i not in d:
             d[i] = 0
-        if type(d[i]) == list:
+        if isinstance(d[i], list):
             d[i] = sum(d[i])
-        else:
-            print(type(d[i]))
-    # for i in range(length):
-    #     if i not in q:
-    #         q[i] = 0
+        if isinstance(q[i], list):
+            q[i] = sum(q[i])
+        if isinstance(d[i], int):
+            d[i] = float(d[i])
+        if isinstance(q[i], int):
+            q[i] = float(q[i])
 
     dot = sum(d[i] * q[i] for i in range(len(q)))
     mag_1 = sum(d[i] ** 2 for i in range(len(q))) ** 0.5
@@ -26,22 +27,19 @@ def cosine(d, q):
         return 0
     return dot / (mag_1 * mag_2)
 
-def tfidf(data, doc, doc_num, total_docs): #word, doc, doc_count, term_count
+def tfidf(data, doc, doc_num, total_docs, query=False): #word, doc, doc_count, term_count
     #data: [('ball', [...])], doc: full text, doc_num: index of text, total_docs: total number of docs
-    term, info = data[0], data[1][0]
-
-    docnum, count, positions = info[0], info[1], info[2]
-    poslist = positions.split(',')
-    if doc_num == 0:
-        docposlist = poslist
+    data = data.split(':')
+    docposlist = data[2].split(',')
+    poslist = doc_num
+    if query:
         doc = str(doc)
     else: # 
         with zipfile.ZipFile(doc, 'r') as zip_ref:
             file_name = zip_ref.namelist()[0]
             with zip_ref.open(file_name) as f:
                 doc = f.read().decode('utf-8', errors='ignore')
-        doc_num = doc_num.split(':')[0]
-        docposlist = positions.split(';')
+        # docposlist = positions.split(';')
     term_count = len(poslist) #number of docs the term appears in
     tf = len(docposlist) / len(doc.split())
     idf = (total_docs / term_count) ** 0.5
@@ -50,19 +48,28 @@ def tfidf(data, doc, doc_num, total_docs): #word, doc, doc_count, term_count
 # hashmap = ChainHashMap()
 stemmer = PorterStemmer()
 def scorer(query, total_docs, docdict, hashmap):
-    query_tfidf = []
+    query_tfidf = {}
     doctfidf = {}
     query_index = 0
     for word in query:
+        # get relevant words that are in any of the documents
         if not word.is_stop and word.is_alpha:
             word = stemmer.stem(word.text)
             data = hashmap.get(word)
             if data is not None: #[('ball', [...])]
-                query_tfidf.append(tfidf(data, query, 0, total_docs))
-                
+                # query tfidf scores
+                n = 0
+                for i in data[1][0].split(';'):
+                    tfidf_score = tfidf(i, query, data[1][0].split(';'), total_docs, query=True)
+                    if word in query_tfidf:
+                        query_tfidf[n].append(tfidf_score)
+                    else:
+                        query_tfidf[n] = [tfidf_score]
+                    n += 1
+                # doc tfidf scores
                 for i in data[1][0].split(';'):
                     text = docdict[i[0]]
-                    tfidf_score = tfidf(data, text, i, total_docs)
+                    tfidf_score = tfidf(i, text, data[1][0].split(';'), total_docs)
                     key = i.split(':')[0]
                     if key in doctfidf:
                         doctfidf[i.split(':')[0]][query_index] = tfidf_score
@@ -94,13 +101,12 @@ def search(paths, docdict=None):
     while True:
         partial_scores.clear()
         positions.clear()
-        query = input("Enter search query: ")
+        query = input("Enter search query: ").strip().lower()
         nlp = spacy.load('en_core_web_sm')
         stemmer = PorterStemmer()
         doc = nlp(query)
         total_docs = hashmap.__len__()
         scorer(doc, total_docs, docdict, hashmap)
-            
         print(partial_scores)
         if partial_scores != {}:
             sorted_results = sorted(partial_scores.items(), key=lambda item: item[1], reverse=True)
@@ -110,20 +116,21 @@ def search(paths, docdict=None):
         else:
             print("No results found.")
 
-
-def prepare_search(paths): #TODO delete?
-    index_path = build_index(paths)
-    docdict = {}
-    n = 1
-    for path in paths:
-        docdict[str(n)] = path
-        n += 1
-    results = search(index_path)
-
 def search_engine():
     paths = input("Directory of input files: ")
     input_path = get_all_files(paths)
     search(input_path)
+
+def search_existing_index():
+    index_path = input("Path to index: ")
+    paths = input("Directory of input files: ")
+    input_path = get_all_files(paths)
+    docdict = {}
+    n = 1
+    for path in input_path:
+        docdict[str(n)] = path
+        n += 1
+    search(index_path, docdict)
 
 inv_index_path = "C:\\Users\\akeen\\Downloads\\New SWE247P project\\inv-index\\inv-index.txt"
 paths = ['C:\\Users\\akeen\\Downloads\\New SWE247P project\\input-files\\aleph.gutenberg.org\\1\\0\\0\\0\\10001\\10001.zip', 'C:\\Users\\akeen\\Downloads\\New SWE247P project\\input-files\\aleph.gutenberg.org\\1\\0\\0\\0\\10002\\10002.zip']
@@ -138,6 +145,9 @@ def get_all_files(directory):
     return file_paths
 
 # input_files_directory = 'C:\\Users\\akeen\\Downloads\\New SWE247P project\\input-files\\aleph.gutenberg.org\\1\\0\\0\\0'
+# input_files_directory = 'C:\Users\akeen\Downloads\New SWE247P project\input-files\aleph.gutenberg.org\1\0\0\0'
 # paths = get_all_files(input_files_directory)
 # prepare_search(paths)
-search_engine()
+
+# search_engine()
+search_existing_index()
