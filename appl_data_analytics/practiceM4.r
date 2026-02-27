b5d9ea20d698c2d06e60715bf1712767f6535ca8
Adam <akeene949@gmail.com>
2026-02-26 21:34:38 -0800

    data 4

--- appl_data_analytics/practiceM4.r
+++ appl_data_analytics/practiceM4.r
@@ -1,4 +1,8 @@
 library(ISLR2)
+library(MASS)
+library(randomForest)
+install.packages("tree")
+library(tree)
 
 df <- na.omit(Auto[, c("horsepower", "weight")])
 
@@ -35,11 +39,11 @@ plot.window(xlim = c(0, 1), ylim = c(0, 1))
 title("Decision Tree")
 
 # y-levels
-y0 <- 0.88  # root split
-y1 <- 0.70  # children of root
-y2 <- 0.52  # grandchildren on left side
-y3 <- 0.34  # leaves under left-left and left-right
-y4 <- 0.52  # leaves under right side (two leaves)
+y0 <- 0.88
+y1 <- 0.70
+y2 <- 0.52
+y3 <- 0.34
+y4 <- 0.52
 
 # x-positions
 x_root <- 0.50
@@ -54,43 +58,43 @@ xR4 <- 0.46
 xR5 <- 0.66
 xR6 <- 0.84
 
-# Root split: horsepower < t1
+# horsepower < t1
 segments(xL, y0, xR, y0)
 segments(xL, y0, xL, y1)
 segments(xR, y0, xR, y1)
-text(x_root, y0 + 0.03, expression(horsepower < t[1]~~"(110)"), cex = 0.9)
+text(x_root, y0 + 0.03, expression(horsepower < t[1]~~": 110"))
 
-# Left child split: weight < t2
+# weight < t2
 segments(xLL, y1, xLR, y1)
 segments(xLL, y1, xLL, y2)
 segments(xLR, y1, xLR, y2)
-text((xLL + xLR) / 2, y1 + 0.03, expression(weight < t[2]~~"(2600)"), cex = 0.9)
+text((xLL + xLR) / 2, y1 + 0.03, expression(weight < t[2]~~": 2600"))
 
-# Left-left split: horsepower < t3 -> R1, R2
+# horsepower < t3
 segments(xR1, y2, xR2, y2)
 segments(xR1, y2, xR1, y3)
 segments(xR2, y2, xR2, y3)
-text((xR1 + xR2) / 2, y2 + 0.03, expression(horsepower < t[3]~~"(80)"), cex = 0.85)
+text((xR1 + xR2) / 2, y2 + 0.03, expression(horsepower < t[3]~~": 80"))
 
-# Left-right split: weight < t4 -> R3, R4
+# weight < t4 -> R3, R4
 segments(xR3, y2, xR4, y2)
 segments(xR3, y2, xR3, y3)
 segments(xR4, y2, xR4, y3)
-text((xR3 + xR4) / 2, y2 + 0.03, expression(weight < t[4]~~"(3200)"), cex = 0.85)
+text((xR3 + xR4) / 2, y2 + 0.03, expression(weight < t[4]~~": 3200"))
 
-# Right child split: weight < t5 -> R5, R6
+# weight < t5 -> R5, R6
 segments(xR5, y1, xR6, y1)
 segments(xR5, y1, xR5, y4)
 segments(xR6, y1, xR6, y4)
-text((xR5 + xR6) / 2, y1 + 0.03, expression(weight < t[5]~~"(3400)"), cex = 0.9)
+text((xR5 + xR6) / 2, y1 + 0.03, expression(weight < t[5]~~": 3400"))
 
-# Leaf labels
-text(xR1, y3 - 0.03, "R1", cex = 0.95)
-text(xR2, y3 - 0.03, "R2", cex = 0.95)
-text(xR3, y3 - 0.03, "R3", cex = 0.95)
-text(xR4, y3 - 0.03, "R4", cex = 0.95)
-text(xR5, y4 - 0.03, "R5", cex = 0.95)
-text(xR6, y4 - 0.03, "R6", cex = 0.95)
+# labels
+text(xR1, y3 - 0.03, "R1")
+text(xR2, y3 - 0.03, "R2")
+text(xR3, y3 - 0.03, "R3")
+text(xR4, y3 - 0.03, "R4")
+text(xR5, y4 - 0.03, "R5")
+text(xR6, y4 - 0.03, "R6")
 
 # 3
 prange = seq(0, 1, 0.01)
@@ -99,11 +103,31 @@ entropy = -(prange * log(prange) + (1 - prange) * log(1 - prange))
 class.err = 1 - pmax(prange, 1 - prange)
 matplot(prange, cbind(gini, entropy, class.err), type = "l", lty = 1, col = c("red" ,"green", "blue"))
 
-# 4
-# a
-tree <- ape::read.tree(text = "(((3:1.5,(10:1,0:1)A:1)B:1,15:2)C:1,5:2)D;")
-tree$node.label <- c("X1 < 1", "X2 < 1", "X1 < 0", "X2 < 0")
+# 7
+# Construct the train and test matrices
+boston_train = sample(dim(Boston)[1], dim(Boston)[1]/2)
+x_train = Boston[boston_train, -14]
+x_test = Boston[-boston_train, -14]
+y_train = Boston[boston_train, 14]
+y_test = Boston[-boston_train, 14]
 
-ggtree(tree, ladderize = FALSE) + scale_x_reverse() + coord_flip() +
-  geom_tiplab(vjust = 2, hjust = 0.5) +
-  geom_text2(aes(label = label, subset = !isTip), hjust = -0.1, vjust = -1)
+p = dim(Boston)[2] - 1
+p_div = p/2
+p_sq = sqrt(p)
+rf_p = randomForest(x_train, y_train, xtest = x_test, ytest = y_test, mtry = p, ntree = 500)
+rf_div = randomForest(x_train, y_train, xtest = x_test, ytest = y_test, mtry = p_div, ntree = 500)
+rf_sq = randomForest(x_train, y_train, xtest = x_test, ytest = y_test, mtry = p_sq, ntree = 500)
+
+plot(1:500, rf_p$test$mse, col = "green", type = "l", xlab = "tree count", ylab = "MSE", ylim = c(10, 19))
+lines(1:500, rf_div$test$mse, col = "red", type = "l")
+lines(1:500, rf_sq$test$mse, col = "blue", type = "l")
+legend("topright", c("m=p", "m=p/2", "m=sqrt(p)"), col = c("green", "red", "blue"), cex = 1, lty = 1)
+
+# 8
+# a.
+seatsTrain=sample(1:nrow(Carseats),nrow(Carseats)*7/10)
+seatsTest=Carseats[-seatsTrain,'Sales']
+# b.
+seatsTree=tree(Sales~.,Carseats,subset=seatsTrain)
+plot(seatsTree)
+text(seatsTree, pretty = 0)
